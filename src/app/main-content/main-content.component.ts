99b9eb78118411b81f5492ed6ea8ed77312e604c
Sebastian Regending <sebastianr@web.de>
2025-01-13 23:06:58 +0100

    added privacy policy

---
+++ src/app/main-content/main-content.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { HeroSectionComponent } from "../hero-section/hero-section.component";
+import { AboutmeSectionComponent } from "../aboutme-section/aboutme-section.component";
+import { SkillsSectionComponent } from "../skills-section/skills-section.component";
+import { ProjectsSectionComponent } from "../projects-section/projects-section.component";
+import { ContactmeSectionComponent } from "../contactme-section/contactme-section.component";
+
+@Component({
+  selector: 'app-main-content',
+  standalone: true,
+  imports: [HeroSectionComponent, AboutmeSectionComponent, SkillsSectionComponent, ProjectsSectionComponent, ContactmeSectionComponent],
+  templateUrl: './main-content.component.html',
+  styleUrl: './main-content.component.scss'
+})
+export class MainContentComponent {
+
+}
