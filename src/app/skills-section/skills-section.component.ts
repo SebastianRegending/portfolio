0192b3702f3851386ecee10090937163a8b04b74
Sebastian Regending <sebastianr@web.de>
2025-01-16 22:09:37 +0100

    skills and projects sections responsiveness

--- src/app/skills-section/skills-section.component.ts
+++ src/app/skills-section/skills-section.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { sharedImports } from '../../app/shared/imports';
 import { LanguageService } from '../shared/language.service';
+import { title } from 'node:process';
 
 @Component({
   selector: 'app-skills-section',
   standalone: true,
   imports: [sharedImports],
   templateUrl: './skills-section.component.html',
-  styleUrl: './skills-section.component.scss'
+  styleUrl: './skills-section.component.scss',
 })
 export class SkillsSectionComponent {
   constructor(private languageService: LanguageService) {}
@@ -15,4 +16,46 @@ export class SkillsSectionComponent {
     this.languageService.useLanguage(language);
   }
 
+  mySkills = [
+    {
+      image: '../../assets/img/html.png',
+      title: 'HTML',
+    },
+    {
+      image: '../../assets/img/css.png',
+      title: 'CSS',
+    },
+    {
+      image: '../../assets/img/javascript.png',
+      title: 'JavaScript',
+    },
+    {
+      image: '../../assets/img/materialdesign.png',
+      title: 'Material Design',
+    },
+    {
+      image: '../../assets/img/typescript.png',
+      title: 'TypeScript',
+    },
+    {
+      image: '../../assets/img/angular.png',
+      title: 'Angular',
+    },
+    {
+      image: '../../assets/img/firebase.png',
+      title: 'Firebase',
+    },
+    {
+      image: '../../assets/img/git.png',
+      title: 'Git',
+    },
+    {
+      image: '../../assets/img/api.png',
+      title: 'REST-API',
+    },
+    {
+      image: '../../assets/img/scrum.png',
+      title: 'SCRUM',
+    }
+  ];
 }
